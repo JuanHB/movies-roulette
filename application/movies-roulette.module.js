3f29ce5aecdbd1ddb27e2a6533f7ac2def96a1b4
JuanHB <juanbiscaia@gmail.com>
2016-02-18 11:16:17 -0200

    Initial js structure

---
+++ application/movies-roulette.module.js
@@ -0,0 +1,15 @@
+/*
+* The app modules wrapper
+* */
+(function(){
+    // ecma6 / jshint
+    'use strict';
+
+    angular
+        .module('MoviesRouletteApp', [
+            /* The Core module,
+            that contains all 3rd part modules*/
+            'MoviesRouletteApp.core'
+        ])
+
+})();
